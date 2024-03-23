8a9b0ab0cdd0a6a82e6dd148922cb0dc288601a6
JJibbz <vkazancev322@gmail.com>
2024-03-24 00:21:27 +0700

    Добавьте файлы проекта.

---
+++ Mod17FinPractice/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod17FinPractice
+{
+    public class Calculator
+    {
+        public void CalcType(Account account, ICalculator calculator)
+        {
+            calculator.CalculateInterest(account);
+        }
+    }
+}
---
+++ Mod17FinPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Mod17FinPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account regularAccount = new Account { Type = "Обычный", Balance = 70000 };
+            Account salaryAccount = new Account { Type = "Зарплатный", Balance = 10000 };
+
+            Calculator calculator = new Calculator();
+
+            
+            ICalculator ordinaryCalculator = new OrdinaryCalculator();
+            ICalculator salaryCalculator = new SalaryCalculator();
+
+            
+            calculator.CalcType(regularAccount, ordinaryCalculator);
+            calculator.CalcType(salaryAccount, salaryCalculator);
+
+            
+            Console.WriteLine($"Regular account interest: {regularAccount.Interest}");
+            Console.WriteLine($"Salary account interest: {salaryAccount.Interest}");
+        }
+    }
+}
